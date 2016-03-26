2a476505a6263ca52d4b1be55cc163f47ed7ca46
Hans Hoffman <hdot@GreenCouch.local>
2016-03-26 14:43:54 -0400

    cleaned up a little

--- collatz_basic.c
+++ collatz_basic.c
@@ -5,8 +5,10 @@ const int MAX_NUM = 1000000;
 
 int main(int argc, const char* argv[]) {
 	int maxIndex = 0, currIterations = 1, maxIterations = 0;
+	unsigned long n;
+
 	for (int i=2; i<=MAX_NUM; i++) {
-		unsigned long n = i;
+		n = i;
 		while (n > 1) {
 			currIterations++;
 			n = (n % 2 == 0) ? n/2 : 3*n+1;
@@ -16,14 +18,9 @@ int main(int argc, const char* argv[]) {
 	}
 	
 	printf("Starting number: %d\n%d ", maxIndex, maxIndex);
-	unsigned long j = maxIndex;
-	while (j > 1) {
-		j = (j % 2 == 0) ? j/2 : 3*j+1;
-		printf("%lu ", j);
+	n = maxIndex;
+	while (n > 1) {
+		n = (n % 2 == 0) ? n/2 : 3*n+1;
+		printf("%lu ", n);
 	}
 }
-//https://hbfs.wordpress.com/2012/05/01/faster-collatz/
-//http://www.ericr.nl/wondrous/techpage.html
-//http://gribblelab.org/CBootcamp/A2_Parallel_Programming_in_C.html
-//http://www.mathblog.dk/project-euler-14/
-//http://code.jasonbhill.com/c/project-euler-problem-14/
